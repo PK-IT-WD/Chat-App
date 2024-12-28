012f12176e93f17ba4264af56441237a394164c2
PK-IT-WD <pradeepkumaritwd@gmail.com>
2024-12-28 11:09:44 +0530

    signup page

---
+++ frontend/app.js
@@ -0,0 +1,29 @@
+const signupForm = document.getElementById('signupForm');
+if (signupForm) {
+    signupForm.addEventListener('submit', async (event)=> {
+        event.preventDefault();
+        const userDetails = {
+            userName: document.getElementById('userName').value.trim(),
+            email: document.getElementById('email').value.trim(),
+            mobileNumber: document.getElementById('mobileNumber').value,
+            password: document.getElementById('password').value.trim()
+        }
+        try {
+            const response = await fetch('/signup', {
+                method: 'POST',
+                headers: {'Content-Type': 'application/json'},
+                body: JSON.stringify(userDetails)
+            });
+            const data = await response.json();
+            if (data.success) {
+                message.innerHTML = `<p>User Details added successfully</p>`;
+                message.style.color = 'green';
+            } else {
+                message.innerHTML = `<p>${data.message}</p>`;
+                message.style.color = 'red';
+            }
+        } catch (err) {
+            console.error('Signup Error:', err);
+        }
+    });
+}
